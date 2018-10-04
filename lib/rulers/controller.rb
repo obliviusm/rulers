2a3a0b8cab6a8e5f285e73ce01b67869da51fa3c
Misha Sebalo <obliviusm@gmail.com>
2018-10-04 18:45:41 +0300

    Add ctrl folder

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -6,8 +6,12 @@ module Rulers
       @env = env
     end
 
+    def controller_name
+      Rulers.to_underscore self.class.to_s.chomp('Controller')
+    end
+
     def render(view_name, locals = {})
-      path = File.join('app', 'views', view_name)
+      path = File.join('app', 'views', controller_name, view_name)
       template = File.read(path)
       eruby = Erubis::Eruby.new(template)
       eruby.result(locals.merge(env: env))
